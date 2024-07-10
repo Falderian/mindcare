3dfc533c6f18cf2249f95e7cc5703f9e33a5c168
Nikita <n.staselovich@codemaker.click>
2024-07-10 06:54:25 +0000

    Add mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "../theme";
+import theme from "../utils/theme";
+import { Toaster } from "react-hot-toast";
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
-  <html lang="en">
+  <html lang="ru">
     <body style={{ margin: 0 }}>
       <AppRouterCacheProvider>
         <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </AppRouterCacheProvider>
+      <Toaster />
     </body>
   </html>
 );
---
+++ src/components/NavigationMenu.tsx
@@ -0,0 +1,9 @@
+import { Drawer } from "@mui/material";
+
+export const NavigationMenu = () => {
+  return (
+    <Drawer variant="permanent" sx={{ width: 300 }}>
+      Mindcare
+    </Drawer>
+  );
+};
